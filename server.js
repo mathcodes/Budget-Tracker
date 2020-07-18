02c185407e9fa5c4bbdd277b8212600f8b6c25f1
Jon Christie <jonpchristie@gmail.com>
2020-07-18 13:38:22 -0400

    heroku and db connections hooked up

--- server.js
+++ server.js
@@ -15,10 +15,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 
+  "mongodb://user1:password1@ds157057.mlab.com:57057/heroku_k4j2gkgv",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
